d084984b5c1df2a29a3e803272e5604f45270016
carlosdiaztl <diaz-alzate@hotmail.com>
2025-06-03 12:39:58 -0500

    swager and readme

--- src/app/utils/cors.ts
+++ src/app/utils/cors.ts
@@ -5,7 +5,8 @@ const corsOptions = {
     "http://localhost:4200",
     "http://127.0.0.1:5500",
     "https://task-manager-cm01icto0-carlosdiaztls-projects.vercel.app",
-    "https://task-manager-ni6yiga12-carlosdiaztls-projects.vercel.app"
+    "https://task-manager-ni6yiga12-carlosdiaztls-projects.vercel.app",
+    "*"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
