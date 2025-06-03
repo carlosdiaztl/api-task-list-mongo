66db12685baa918b36033e64ca67a648809a4428
carlosdiaztl <diaz-alzate@hotmail.com>
2025-06-02 23:12:37 -0500

    cros

--- src/app/utils/cors.ts
+++ src/app/utils/cors.ts
@@ -1,10 +1,15 @@
 // enable CORS - Cross Origin Resource Sharing
 const corsOptions = {
-    origin: ["http://localhost:5000", "http://localhost:4200", "http://127.0.0.1:5500"],
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization", "Accept"],
-  };
-  
-  
-  export { corsOptions };
+  origin: [
+    "http://localhost:5000",
+    "http://localhost:4200",
+    "http://127.0.0.1:5500",
+    "https://task-manager-cm01icto0-carlosdiaztls-projects.vercel.app",
+    "https://task-manager-cm01icto0-carlosdiaztls-projects.vercel.app",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization", "Accept"],
+};
+
+export { corsOptions };
