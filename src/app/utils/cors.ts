2aa5aff8c6cb18215ec91f5023fe4c8e4ff2a3a7
carlosdiaztl <diaz-alzate@hotmail.com>
2025-06-03 13:06:13 -0500

    cors

--- src/app/utils/cors.ts
+++ src/app/utils/cors.ts
@@ -6,7 +6,7 @@ const corsOptions = {
     "http://127.0.0.1:5500",
     "https://task-manager-cm01icto0-carlosdiaztls-projects.vercel.app",
     "https://task-manager-ni6yiga12-carlosdiaztls-projects.vercel.app",
-    "task-manager-o0hpjzhbc-carlosdiaztls-projects.vercel.app"
+    "https://task-manager-o0hpjzhbc-carlosdiaztls-projects.vercel.app"
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
